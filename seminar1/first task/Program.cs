929bf0a4bbd58fc6e8f769a0f3420617ead22f36
Kirill <you@example.com>
2022-12-15 20:29:29 +0300

    added condition

--- seminar1/first task/Program.cs
+++ seminar1/first task/Program.cs
@@ -3,3 +3,9 @@ int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Enter B");
 int b = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Pow(b,2) == a) {
+    Console.WriteLine("yes");
+} else {
+    Console.WriteLine("no");
+}
