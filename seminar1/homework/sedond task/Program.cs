72c9041405a669b59bcb900e39b385539bdc09ad
Kirill <you@example.com>
2022-12-16 17:23:14 +0300

    adding a solution to the task

---
+++ seminar1/homework/sedond task/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter first number");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    Console.Write("Max = {0}, ",firstNumber);
+    Console.Write("Min = {0}, ",secondNumber);
+} else 
+{
+    Console.Write("Min = {0}, ",firstNumber);
+    Console.Write("Max = {0}, ",secondNumber);
+}
