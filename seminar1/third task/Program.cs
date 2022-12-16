a4798d4fa9d921a603d3d188212c09a588f7ce0b
Kirill <you@example.com>
2022-12-16 14:18:44 +0300

    Added cycel for resolve task

---
+++ seminar1/third task/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter number:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int b = -(a*2);
+
+while (i <= (a*2)) 
+{
+    Console.Write(b);
+    i = i++;
+    b = b++;
+}
