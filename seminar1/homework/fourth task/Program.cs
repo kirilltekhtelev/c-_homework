a493bf2e4ba830c7c81e2db974d283e02ae215ba
Kirill <you@example.com>
2022-12-16 17:33:29 +0300

    adding a solution to the fourth task

---
+++ seminar1/homework/fourth task/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter first number:");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number:");
+
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number:");
+
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if(secondnumber > firstNumber || secondnumber > thirdNumber)
+{
+    max = thirdNumber;
+} 
+else 
+{
+    if (thirdNumber > firstNumber)
+    {
+        max = thirdNumber;
+    }
+    else
+    {
+        max = firstNumber;
+    }
+}
