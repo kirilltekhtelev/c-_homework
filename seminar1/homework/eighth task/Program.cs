5bcda8f674fc539b28de594c038f2058171a04e2
Kirill <you@example.com>
2022-12-20 19:24:32 +0300

    adding a solutin for the task

---
+++ seminar1/homework/eighth task/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter number");
+int maxLineNumber = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i<=maxLineNumber)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i += 1;
+}
