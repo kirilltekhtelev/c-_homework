0502e546916702553e3c7ca245ac367e4dbcf7a0
Kirill <you@example.com>
2022-12-20 19:14:09 +0300

    adding a solution for the task

---
+++ seminar1/homework/sixth task/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number");
+int evenNumber = Converte.ToInt32(Console.ReadLine());
+
+if(evenNumber % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else 
+{
+    Console.WriteLine("No");
+}
