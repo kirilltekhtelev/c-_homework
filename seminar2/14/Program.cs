c74b7b6436c9fb04a778d038ad4e91bd77211840
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-22 14:27:15 +0300

    adding a solution to the task

---
+++ seminar2/14/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
