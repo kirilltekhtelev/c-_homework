94d2c2ba2ab7d1d55cf88a88dfed2ce484afcb9d
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-22 14:47:21 +0300

    adding a solution to the task

---
+++ seminar2/homework/13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter number");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number / 100 % 10 != 0) {
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("No third digit");
+}
