fc7bc0f7b832d72d908f9e6eb01ec6fdd4cc7c45
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-22 14:54:45 +0300

    adding a solution to the 15 task

---
+++ seminar2/homework/15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter day of the week as a number");
+
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayOfWeek >= 6 && dayOfWeek <= 7)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
