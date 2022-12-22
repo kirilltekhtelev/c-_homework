92751834d7f8414e9b6f77e84bbac9a2068630fe
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-22 14:34:21 +0300

    adding a solution to the 16 task

---
+++ seminar2/16/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter first number");
+
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(Math.Pow(firstNumber, 2) == secondNumber || Math.Pow(secondNumber, 2) == firstNumber)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
