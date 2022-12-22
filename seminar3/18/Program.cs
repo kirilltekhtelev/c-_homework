a3b342ec05a3a113a4496d26c1ec2279a27b9874
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-22 20:31:52 +0300

    added a solution to the 18 task

---
+++ seminar3/18/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter quarter number");
+
+int quarterNumber = Convert.ToInt32(Console.ReadLine());
+
+switch(quarterNumber)
+{
+    case 1: 
+        Console.WriteLine("x>0;y>0");
+        break;
+    case 2: 
+        Console.WriteLine("x<0;y>0");
+        break;
+    case 3: 
+        Console.WriteLine("x<0;y<0");
+        break;
+    case 4: 
+        Console.WriteLine("x>0;y<0");
+        break;
+}
