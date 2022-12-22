f01b39a50ec88ed3dcc532195aee5c076cabff10
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-22 14:19:38 +0300

    adding a solution for the 12 task

---
+++ seminar2/12/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int remaiderOfDivision = 0;
+
+if (firstNumber % secondnumber == 0)
+{
+    Console.WriteLine("Multiple");
+}
+else
+{
+    remaiderOfDivision = firstNumber % secondnumber;
+    Console.WriteLine($"Not multiple, remainder of the division: {remaiderOfDivision}");
+}
