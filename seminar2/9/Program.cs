398a7987fe981a3eb0810f322a5228b2309d0de3
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-22 12:45:10 +0300

    adding a soultion for 9 task

---
+++ seminar2/9/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = new Random().Next(10,100);
+
+int firstDigitOfNumber = number / 10;
+int secondDigitOfNumber = number %10;
+
+Console.WriteLine(number);
+
+if (firstDigitOfNumber > secondDigitOfNumber)
+{
+    Console.WriteLine($"max is: {firstDigitOfNumber}");
+}
+else
+{
+    Console.WriteLine($"max is: {secondDigitOfNumber}");
+}
