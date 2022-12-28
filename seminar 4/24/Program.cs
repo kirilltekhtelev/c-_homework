cc35e53bdb1d6862a3662ba5c7cd0d000bb62449
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-28 12:58:46 +0300

    added a solution to the 24 task

---
+++ seminar 4/24/Program.cs
@@ -0,0 +1,20 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int sumFromOneToA (int a)
+{
+    int s = 0;
+    for(int i = 1; i <= a; i++)
+    {
+        s+=i;
+    }
+    return s;
+}
+
+int a = readInt("Enter a number");
+
+int result = sumFromOneToA(a);
+Console.Write(result);
--- seminar3/22/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.WriteLine("Enter N");
-
-int n = Convert.ToInt32(Console.ReadLine());
-
-for(int i = 1; i <= n; i++)
-{
-    Console.WriteLine(Math.Pow(i,2));
-}
