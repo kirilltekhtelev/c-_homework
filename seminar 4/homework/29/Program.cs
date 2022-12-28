b725e768a8634c54b52c87e4543ed15ba6c2b091
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-28 17:31:47 +0300

    added a solution to the 29 task

---
+++ seminar 4/homework/29/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] readIntArr (string message)
+{
+    int[] arr = new int[8];
+    for (int i = 0;i < 8; i++ )
+    {
+        Console.WriteLine($"{message} {i+1}");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return arr;
+}
+
+int[] arrOfEigthNumber = readIntArr("Enter number: ");
+
+Console.WriteLine($"{string.Join(",", arrOfEigthNumber)}");
