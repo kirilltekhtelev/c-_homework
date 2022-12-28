ced5354a520359fbb5c2b3959df4615772bec087
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-28 15:43:50 +0300

    added a solution to the 30 task

---
+++ seminar 4/30/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] arrayOfOneOrZero = new int[8];
+for (int i = 0; i < arrayOfOneOrZero.Length; i++)
+{
+arrayOfOneOrZero[i] = new Random().Next(0,2);
+}
+
+Console.WriteLine(string.Join(", ", arrayOfOneOrZero));
