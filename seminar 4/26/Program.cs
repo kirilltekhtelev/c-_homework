426948e1ac83e2c596fbebd89a6c4891c36f4f76
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-28 13:12:14 +0300

    added a solution to the 26 task

---
+++ seminar 4/26/Program.cs
@@ -0,0 +1,25 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int countDigitOfNumber (int number)
+{
+    int i = 0;
+    do
+    {
+        if(number % 10 >= 0)
+        {
+            i += 1;
+            number /= 10;
+        }
+    }
+    while (number != 0);
+    return i;
+}
+
+int number = readInt("Enter number");
+int result = countDigitOfNumber(number);
+
+Console.WriteLine(result);
