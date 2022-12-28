3cb86dc07d9b09729121e2a23d4825620734ed34
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-28 15:31:29 +0300

    added a solution to the 28 task

---
+++ seminar 4/28/Program.cs
@@ -0,0 +1,19 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int factorialN (int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * factorialN(n-1);
+}
+
+int n = readInt("Enter N");
+
+int factorialOfNumber = factorialN(n);
+
+Console.WriteLine(factorialOfNumber);
