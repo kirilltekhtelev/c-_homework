9f067568c4b6a550f6c9ca810c1d89c642829736
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-28 15:55:00 +0300

    added a solution to the 25 task

---
+++ seminar 4/homework/25/Program.cs
@@ -0,0 +1,20 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int powAToDegreeB (int a, int b)
+{
+    if (b == 0)
+        return 1;
+    else 
+        return a * powAToDegreeB(a, b-1);
+}
+
+int a = readInt("Enter A");
+int b = readInt("enter B");
+
+int result = powAToDegreeB(a, b);
+
+Console.WriteLine(result);
