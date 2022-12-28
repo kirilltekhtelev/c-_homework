0768e42d438a8c6f1ec8cfeef3a4699073f2615c
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2022-12-28 17:16:08 +0300

    added a solution to the 27 task

---
+++ seminar 4/homework/27/Program.cs
@@ -0,0 +1,25 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int countSumOfDigitOfNumber (int number)
+{
+    int s = 0;
+    do
+    {
+        if(number % 10 >= 0)
+        {
+            s += number % 10;
+            number /= 10;
+        }
+    }
+    while (number != 0);
+    return s;
+}
+
+int number = readInt("Enter number");
+int result = countSumOfDigitOfNumber(number);
+
+Console.WriteLine(result);
