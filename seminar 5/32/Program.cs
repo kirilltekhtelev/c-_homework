954a96dceb4097ef9178f50038d6ffba5f6992d9
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2023-01-01 22:34:26 +0300

    added a solution to the 32 task

---
+++ seminar 5/32/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] createRandomMassive ()
+{
+    int[] arr = new int[10];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100, 101);
+    }
+    return arr;
+}
+
+int[] invertMassive (int[] arr)
+{
+    for(int i = 0; i < 10; i++)
+    {
+        arr[i] *= -1;
+    }
+    return arr;
+}
+
+int[] arr = createRandomMassive();
+
+Console.WriteLine($"original: {string.Join(",",arr)}");
+
+invertMassive(arr);
+
+Console.WriteLine($"invert: {string.Join(",",arr)}");
