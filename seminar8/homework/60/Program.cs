71e75fe46bb7e9e7b5ba81cf429a2af690a4d95b
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2023-01-13 12:00:48 +0300

    added a solution to the 60 task

---
+++ seminar8/homework/60/Program.cs
@@ -0,0 +1,40 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,,] createMatrix ()
+{
+    int[,,] matrix = new int[2,2,2];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i,j,z] = readInt("enter number");
+            }
+        }
+    }
+    return matrix;
+}
+
+void printMatrix (int[,,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for(int z = 0; z < matrix.GetLength(2); z++)
+            {
+                Console.Write($"{matrix[i,j,z]} ({i},{j},{z})");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] matrix = createMatrix();
+
+printMatrix(matrix);
