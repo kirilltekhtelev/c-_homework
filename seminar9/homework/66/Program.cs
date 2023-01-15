df28670e22b5b951091c6409850627c898349f14
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2023-01-15 15:30:01 +0300

    added a solution to the 66 task

---
+++ seminar9/homework/66/Program.cs
@@ -0,0 +1,20 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int printNumber (int n, int m)
+{
+    if (n > m)
+        return 0;
+    else
+        return n + printNumber(n + 1, m);
+}
+
+int n = readInt("Enter N:");
+int m = readInt("Enter M:");
+
+int sum = printNumber(n, m);
+
+Console.WriteLine(sum);
