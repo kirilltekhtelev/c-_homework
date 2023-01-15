1cf719c4eb5a6588d60730dfb5095abc6fb1db82
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2023-01-15 15:16:45 +0300

    added a solution to the 64 task

---
+++ seminar9/homework/64/Program.cs
@@ -0,0 +1,20 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int printNumber (int n)
+{
+    if (n == 0)
+        return 1;
+    else
+    {
+        Console.Write(n + " ");
+        return printNumber(n-1);
+    }
+}
+
+int n = readInt("Enter N:");
+
+printNumber(n);
