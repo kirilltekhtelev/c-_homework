1a8a788e215fee126680741e6aeb8e15ab64a438
kirilltekhtelev <tekhtelev.kirill@gmail.com>
2023-01-15 19:26:17 +0300

    added a solution to the 68 task

---
+++ seminar9/homework/68/Program.cs
@@ -0,0 +1,24 @@
+﻿int readInt (string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int findAckerman (int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+    if ((n == 0) && (m > 0))
+      return findAckerman(m - 1, 1);
+    else
+      return findAckerman(m - 1, findAckerman(m, n - 1));
+}
+
+
+int m = readInt("Enter M:");
+int n = readInt("Enter N:");
+
+int res = findAckerman(m, n);
+
+Console.WriteLine(res);
